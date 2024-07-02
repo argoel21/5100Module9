8093595131718aed624eace4abc07da48e8cb511
Anjali Goel <anjaligoel@Anjalis-MacBook-Pro.local>
2024-07-02 11:00:54 -0400

    test

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(
     name='Test',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/argoel21/5100Module9',
     author='Anjali Goel',
     author_email='arg8qqv@virginia.edu',
     description='Description of my package',
-    packages=find_packages(),    
-    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    packages=['mypackage'],    
 )
